2dd2b60f36d59799ed8386501ed322ab74e82765
Daniel Trompetter <danieltrompetter@gmail.com>
2025-09-16 14:17:58 +0200

    cleaned up two and three

--- lib/src/features/dirty_screen_2/presentation/dirty_screen_two.dart
+++ lib/src/features/dirty_screen_2/presentation/dirty_screen_two.dart
@@ -8,88 +8,46 @@ class DirtyScreenTwo extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Dirty Screen #2"),
       ),
-      body: SingleChildScrollView(
+      body: const SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(
           children: [
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.greenAccent),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.eco),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'Box A',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.greenAccent),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.eco),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'Box B',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.greenAccent),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.eco),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'Box C',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.greenAccent),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.eco),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'Box D',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
+            MyBox(title: 'Box A'),
+            MyBox(title: 'Box B'),
+            MyBox(title: 'Box C'),
+            MyBox(title: 'Box D'),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MyBox extends StatelessWidget {
+  final String title;
+  const MyBox({
+    super.key, required this.title
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.greenAccent),
+      margin: const EdgeInsets.all(4),
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          const Icon(Icons.eco),
+          const SizedBox(
+            width: 8,
+          ),
+          Text(
+            title,
+            style: const TextStyle(fontSize: 20, color: Colors.white),
+          )
+        ],
       ),
     );
   }
--- lib/src/features/dirty_screen_3/presentation/dirty_screen_three.dart
+++ lib/src/features/dirty_screen_3/presentation/dirty_screen_three.dart
@@ -8,88 +8,46 @@ class DirtyScreenThree extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Dirty Screen #3"),
       ),
-      body: SingleChildScrollView(
+      body: const SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(
           children: [
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.orangeAccent),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.warning),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'Alert 1',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.orangeAccent),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.warning),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'Alert 2',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.orangeAccent),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.warning),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'Alert 3',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.orangeAccent),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.warning),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'Alert 4',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
+            MyAlert(title: 'Alert 1'),
+            MyAlert(title: 'Alert 2'),
+            MyAlert(title: 'Alert 3'),
+            MyAlert(title: 'Alert 4'),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MyAlert extends StatelessWidget {
+  final String title;
+  const MyAlert({
+    super.key, required this.title
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.orangeAccent),
+      margin: const EdgeInsets.all(4),
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          const Icon(Icons.warning),
+          const SizedBox(
+            width: 8,
+          ),
+          Text(
+            title,
+            style: const TextStyle(fontSize: 20, color: Colors.white),
+          )
+        ],
       ),
     );
   }
